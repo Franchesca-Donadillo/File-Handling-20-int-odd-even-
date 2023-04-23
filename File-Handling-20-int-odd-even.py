c7e2442c94c614ac426c8c34b49841ff2d300aa1
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-24 02:09:38 +0800

    created num txt file

--- File-Handling-20-int-odd-even.py
+++ File-Handling-20-int-odd-even.py
@@ -3,5 +3,10 @@
 # Program 1 - 20 Integers, Odd/ Even
 
 # create a text file named numbers.txt
+with open("numbers.txt", "a") as file_numbers:
+    
+    int_num = input("Please enter 20 integer numbers (use comma): ")
+
+    file_numbers.write(str(int_num.split(",")))
 # create a text file named even.txt
 # create a text file named odd.txt
