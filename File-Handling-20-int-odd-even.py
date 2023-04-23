a325c0c6f6241b5594f20002eb67518298081abd
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-24 00:44:18 +0800

    initial pseudocode

---
+++ File-Handling-20-int-odd-even.py
@@ -0,0 +1,7 @@
+# Franchesca Marie U. Donadillo
+# BSCPE 1-5
+# Program 1 - 20 Integers, Odd/ Even
+
+# create a text file named numbers.txt
+# create a text file named even.txt
+# create a text file named odd.txt
