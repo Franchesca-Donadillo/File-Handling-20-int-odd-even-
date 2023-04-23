6e033d6d32819363d73c3d580afa8a1746e5e719
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-24 03:06:14 +0800

    not working even.txt

--- File-Handling-20-int-odd-even.py
+++ File-Handling-20-int-odd-even.py
@@ -3,10 +3,15 @@
 # Program 1 - 20 Integers, Odd/ Even
 
 # create a text file named numbers.txt
-with open("numbers.txt", "a") as file_numbers:
+with open("numbers.txt", "w") as file_numbers:
     
     int_num = input("Please enter 20 integer numbers (use comma): ")
 
     file_numbers.write(str(int_num.split(",")))
+
 # create a text file named even.txt
+with open("numbers.txt", "r") as file_numbers, open("even.txt", "w") as file_even:
+    for line in file_numbers:
+        line = (chr(48))
+        file_even.write(line + 2)
 # create a text file named odd.txt
