474d7ace18b1b06c80c97d3a82102571bb4dfda8
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-25 07:36:29 +0800

    no errors but text didn't appear

--- File-Handling-20-int-odd-even.py
+++ File-Handling-20-int-odd-even.py
@@ -2,16 +2,19 @@
 # BSCPE 1-5
 # Program 1 - 20 Integers, Odd/ Even
 
-# create a text file named numbers.txt
-with open("numbers.txt", "w") as file_numbers:
-    
-    int_num = input("Please enter 20 integer numbers (use comma): ")
-
-    file_numbers.write(str(int_num.split(",")))
-
+# read a text file named numbers.txt
 # create a text file named even.txt
-with open("numbers.txt", "r") as file_numbers, open("even.txt", "w") as file_even:
-    for line in file_numbers:
-        line = (chr(48))
-        file_even.write(line + 2)
 # create a text file named odd.txt
+with open("numbers.txt", "r") as file_numbers, open("even.txt", "w") as file_even, open("odd.txt", "w") as file_odd:
+    read_num = file_numbers.readlines()
+    
+    for line in file_numbers:
+        num = read_num[line]
+        if num % 2 == 0:
+            file_even.write(num)
+            line += 1
+            
+        elif num % 2 == 1:
+            file_odd.write(num)
+            line += 1
+        
