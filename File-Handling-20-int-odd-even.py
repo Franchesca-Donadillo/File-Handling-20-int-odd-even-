9cb943b656b721b50c1ab44aca465362aca036ed
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-26 01:16:42 +0800

    pyfiglet

--- File-Handling-20-int-odd-even.py
+++ File-Handling-20-int-odd-even.py
@@ -2,6 +2,11 @@
 # BSCPE 1-5
 # Program 1 - 20 Integers, Odd/ Even
 
+# import pyfiglet
+import pyfiglet
+
+
+
 # read a text file named numbers.txt
 # create a text file named even.txt
 # create a text file named odd.txt
@@ -19,4 +24,6 @@ with open("numbers.txt", "r") as file_numbers, open("even.txt", "w") as file_eve
             file_odd.write(f"Odd #{i}: " + str(num) + "\n")
             i += 0
 
-print("\nCongratulations!\nOdd and even numbers are now in their respective text file. Check it out!\n")
+congrats = pyfiglet.figlet_format("Congrats! ", font = "slant")
+print(congrats)
+print("\nOdd and even numbers are now in their respective text file. Check it out!\n")
