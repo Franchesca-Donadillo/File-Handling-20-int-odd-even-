918b7ed00006ba6e289092879bf2a4a0285808fb
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-25 10:31:59 +0800

    Yey working

--- File-Handling-20-int-odd-even.py
+++ File-Handling-20-int-odd-even.py
@@ -6,15 +6,12 @@
 # create a text file named even.txt
 # create a text file named odd.txt
 with open("numbers.txt", "r") as file_numbers, open("even.txt", "w") as file_even, open("odd.txt", "w") as file_odd:
-    read_num = file_numbers.readlines()
-    
+    # iterate through numbers.txt    
     for line in file_numbers:
-        num = read_num[line]
+        num = int(line)
         if num % 2 == 0:
-            file_even.write(num)
-            line += 1
-            
+            file_even.write(str(num) + "\n")
+                        
         elif num % 2 == 1:
-            file_odd.write(num)
-            line += 1
-        
+            file_odd.write(str(num) + "\n")
+            
